16dc65d233dbd21ad2b4706206392da6d16d3f72
Ishi Agrawal <55421825+agrawish@users.noreply.github.com>
2022-04-27 21:06:33 -0700

    Adding final senior project code
    Code contains HTML, Bootstrap, JS, PHP, PostgreSQL, Heroku hosting, Heroku MemCachier

---
+++ flashclash_demo.php
@@ -0,0 +1,170 @@
+<?php 
+ session_start();
+ $connection = pg_connect(getenv("DATABASE_URL"));
+ if (!$connection)
+ { echo("error connecting..."); }
+ include('checkAccess.php');
+?>
+<!DOCTYPE html>
+<html>
+ <head>
+  <link href="main.css" rel="stylesheet" type="text/css"/>
+  <script src="jquery.min.js"></script>
+  <script src="source.js"></script>
+ </head>
+ 
+ <body>
+  <div id="cardArea"></div>
+  <div id="buttonArea"></div>
+  </div>
+ </body>
+</html>
+
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+<style>
+canvas {
+    border:1px solid #d3d3d3;
+    background-color: #f1f1f1;
+}
+</style>
+</head>
+<h1> Flash Clash </h>
+<body onload="startGame()">
+<script>
+
+var myGamePiece;
+var myObstacles = [];
+
+
+function startGame() {
+    myGamePiece = new component(40, 40, "yellow", 10, 120);
+    myGameArea.start();
+}
+
+var myGameArea = {
+    canvas : document.createElement("canvas"),
+    start : function() {
+        this.canvas.width = 650;
+        this.canvas.height = 400;
+        this.context = this.canvas.getContext("2d");
+        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+        this.frameNo = 0;
+        this.interval = setInterval(updateGameArea, 20);
+		window.addEventListener('keydown', function (e) {
+            myGameArea.key = e.keyCode;
+        })
+        window.addEventListener('keyup', function (e) {
+            myGameArea.key = false;
+        })
+        },
+    clear : function() {
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    },
+    stop : function() {
+        clearInterval(this.interval);
+    }
+}
+//<!--try to use lightning bolt figure-->
+function component(width, height, color, x, y) {
+    this.width = width;
+    this.height = height;
+    this.speedX = 0;
+    this.speedY = 0;    
+    this.x = x;
+    this.y = y;    
+    this.update = function() {
+        ctx = myGameArea.context;
+        ctx.fillStyle = color;
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
+    this.newPos = function() {
+        this.x += this.speedX;
+        this.y += this.speedY;        
+    }    
+    this.crashWith = function(otherobj) {
+        var myleft = this.x;
+        var myright = this.x + (this.width);
+        var mytop = this.y;
+        var mybottom = this.y + (this.height);
+        var otherleft = otherobj.x;
+        var otherright = otherobj.x + (otherobj.width);
+        var othertop = otherobj.y;
+        var otherbottom = otherobj.y + (otherobj.height);
+        var crash = true;
+        if ((mybottom < othertop) || (mytop > otherbottom) || (myright < otherleft) || (myleft > otherright)) {
+            crash = false;
+        }
+        return crash;
+    }
+}
+
+function updateGameArea() {
+    var x, height, gap, minHeight, maxHeight, minGap, maxGap;
+    for (i = 0; i < myObstacles.length; i += 1) {
+        if (myGamePiece.crashWith(myObstacles[i])) {
+            myGameArea.stop();
+            return;
+        } 
+    }
+    myGameArea.clear();
+	myGamePiece.speedX = 0;
+    myGamePiece.speedY = 0;    
+    if (myGameArea.key && myGameArea.key == 37) {myGamePiece.speedX = -1; }
+    if (myGameArea.key && myGameArea.key == 39) {myGamePiece.speedX = 1; }
+    if (myGameArea.key && myGameArea.key == 38) {myGamePiece.speedY = -1; }
+    if (myGameArea.key && myGameArea.key == 40) {myGamePiece.speedY = 1; }
+    myGamePiece.newPos();    
+    myGamePiece.update();
+	
+    myGameArea.frameNo += 1;
+	//<!--everyinterval is spacing in between obstacles-->
+    if (myGameArea.frameNo == 1 || everyinterval(50)) {
+        x = myGameArea.canvas.width;
+        minHeight = 100;
+        maxHeight = 200;
+        height = Math.floor(Math.random()*(maxHeight-minHeight+1)+minHeight);
+        minGap = 100;
+        maxGap = 200;
+        gap = Math.floor(Math.random()*(maxGap-minGap+1)+minGap);
+		//<!--(width,...)-->
+        myObstacles.push(new component(50, height, "gray", x, 0));
+        myObstacles.push(new component(50, x - height - gap, "gray", x, height + gap));
+    }
+    for (i = 0; i < myObstacles.length; i += 5) {
+        myObstacles[i].x += -1;
+        myObstacles[i].update();
+    }
+    myGamePiece.newPos();    
+    myGamePiece.update();
+}
+
+function everyinterval(n) {
+    if ((myGameArea.frameNo / n) % 1 == 0) {return true;}
+    return false;
+}
+
+function moveup() {
+    myGamePiece.speedY = -1; 
+}
+
+function movedown() {
+    myGamePiece.speedY = 1; 
+}
+
+function moveleft() {
+    myGamePiece.speedX = -1; 
+}
+
+function moveright() {
+    myGamePiece.speedX = 1; 
+}
+
+function clearmove() {
+    myGamePiece.speedX = 0; 
+    myGamePiece.speedY = 0; 
+}
+</script>
+
+</body>
+</html>
